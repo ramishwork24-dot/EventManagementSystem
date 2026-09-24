690cdbccf0589bdccc21210a67b38e149807ea41
Muhammad Ramish <ramishwork24@gmail.com>
2026-09-24 15:24:42 +0500

    chore: implement static files configuration and remove redundant files.

--- event_engine/settings.py
+++ event_engine/settings.py
@@ -126,6 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+# Media files configuration
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
