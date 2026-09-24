702269305cfec398fc3791c5368040f18ff28186
Muhammad Ramish <ramishwork24@gmail.com>
2026-09-24 20:39:25 +0500

    feat: implement templates base.html, event_home.html and configure template folder routing in settings.py

--- event_engine/settings.py
+++ event_engine/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'event_engine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
